07dc3d03cdd2010b80fb1c5dbf48342d906d48ca
giusscos <giu.cosenza08@gmail.com>
2022-10-13 15:08:00 +0200

    Minor fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,8 @@ class HomeController extends Controller
 {
     public function index() {
 
-        $trains = Train::where('orario_di_partenza', 'LIKE','%2022-10-12%')->get();
+        $trains = Train::where('orario_di_partenza', 'LIKE','%2022-10-12%')->where('cancellato', 0)->get();
+        // $trains = Train::all();
 
         return view('home', compact('trains'));
     }
